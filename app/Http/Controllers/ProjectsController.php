f23a4e614494ce741c9f2a3d3e249011ced0312f
An <anbn247@gmail.com>
2019-12-11 23:55:03 +0700

    Form Handling and CSRF Protection

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -16,4 +16,22 @@ class ProjectsController extends Controller
         // return $projects;
         return view('projects.index', compact('projects'));
     }
+
+    public function create()
+    {
+        return view('projects.create');
+    }
+
+    public function store()
+    {
+        // return request()->all();
+        $project = new Project();
+
+        $project->title = request('title');
+        $project->description = request('description');
+
+        $project->save();
+
+        return redirect('/projects');
+    }
 }
